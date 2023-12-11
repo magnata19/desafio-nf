7cd725b8c1373d9afc3d4a77c593054d9a45c664
Davidson <dehpacifico@gmail.com>
2023-12-11 13:03:48 -0300

    estruturando projeto

---
+++ src/main/java/br/com/desafionf/desafionf/DesafioNfApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafionf.desafionf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafionf/desafionf/DesafioNfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafionf.desafionf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
