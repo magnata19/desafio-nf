58eb7550b74216d0e9e1edf736d0135df344fe8e
Davidson <dehpacifico@gmail.com>
2023-12-12 18:51:42 -0300

    banco

--- src/main/java/br/com/desafionf/desafionf/controle/NotaFiscalControle.java
+++ src/main/java/br/com/desafionf/desafionf/controle/NotaFiscalControle.java
@@ -2,6 +2,7 @@ package br.com.desafionf.desafionf.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import br.com.desafionf.desafionf.modelo.NotaFiscalModelo;
 import br.com.desafionf.desafionf.servico.NotaFiscalServico;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class NotaFiscalControle {
     
     @Autowired
